5cefadaaee39798d9b8203c1bf0f618d0efbc87e
Ame-ReIori <Ame_ReIori@buaa.edu.cn>
2019-12-13 17:09:37 +0800

    finish user test

--- src/main/java/com/yygq/demo/controller/UserController.java
+++ src/main/java/com/yygq/demo/controller/UserController.java
@@ -28,43 +28,44 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value ="/greeting")
+    @RequestMapping(value ="/user/greeting")
     public String test(){
-        return "test succeed";
+        return "I am the bone of my sword. <br> Steel is my body, and fire is my blood. <br> I have created over a thousand blades. <br> Unknown to Death. <br> Nor known to Life. <br> Have withstood pain to create many weapons. <br> Yet, those hands will never hold anything. <br> So as I pray, unlimited blade works.";
     }
 
-    @RequestMapping(value = "/test/insert")
-    public Dict insert(@RequestParam(value = "username")String username, @RequestParam(value = "password")String password, @RequestParam(value = "phonenumber")String phonenumber, @RequestParam(value = "identitynumber")String identitynumber){
+    @RequestMapping(value = "/user/insert")
+    public Dict insert(@RequestParam(value = "username")String username, @RequestParam(value = "password")String password, @RequestParam(value = "phone")String phone){
         User user = new User();
         user.setName(username);
         user.setPassword(password);
-        user.setPhone(phonenumber);
-        user.setIdentityNumber(identitynumber);
+        user.setPhone(phone);
         user.setMoney(0L);
+        user.setStatus(1);
 
         Boolean insert = userService.insert(user);
         return Dict.create().set("code", insert ? 200 : 500).set("msg", insert ? "success" : "failure").set("data", insert ? user : null);
     }
 
-    @RequestMapping(value = "/test/delete")
-    public Dict delete(@RequestParam(value = "id")Long id){
-        Boolean delete = userService.delete(id);
+    @RequestMapping(value = "/user/delete")
+    public Dict delete(@RequestParam(value = "userId")Long userId){
+        Boolean delete = userService.delete(userId);
         return Dict.create().set("code", delete ? 200 : 500).set("msg", delete ? "success" : "failure");
     }
 
-    @RequestMapping(value = "/test/update")
-    public Dict update(@RequestParam(value = "username")String username, @RequestParam(value = "id")Long id){
+    @RequestMapping(value = "/user/update")
+    public Dict update(@RequestParam(value = "realname")String realName, @RequestParam(value = "identitynumber")String identityNumber, @RequestParam(value = "userId")Long userId){
         User user = new User();
 
-        user.setName(username);
+        user.setRealName(realName);
+        user.setIdentityNumber(identityNumber);
 
-        Boolean update = userService.update(user, id);
+        Boolean update = userService.update(user, userId);
         return Dict.create().set("code", update ? 200 : 500).set("msg", update ? "success" : "failure").set("data", update ? user : null);
     }
 
-    @GetMapping(value = "/user/{id}")
-    public Dict getUser(@PathVariable Long id){
-        User user = userService.getUser(id);
+    @GetMapping(value = "/user/id")
+    public Dict getUser(@RequestParam(name = "userId")Long userId){
+        User user = userService.getUser(userId);
         return Dict.create().set("code", 200).set("msg", "success").set("data", user);
     }
 
@@ -74,7 +75,7 @@ public class UserController {
         return Dict.create().set("code", 200).set("msg", "success").set("data", userList);
     }
 
-    @RequestMapping("/getusertest")
+    @RequestMapping("/user/get")
     public Dict getUsers(@RequestParam(value = "username")String username){
         User user = new User();
         user.setName(username);
